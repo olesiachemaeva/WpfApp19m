c698d10a9ca343fa1632decb22d9608e783d4980
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2022-02-25 20:41:40 +1000

    Добавьте файлы проекта.

---
+++ WpfApp19m/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19m.Models;
+using WpfApp19m.Views;
+
+namespace WpfApp19m.ViewModels
+{
+    //class MainWindow
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        void OnyPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnyPropertyChanged();
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnyPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Ariph.Add(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0)
+                return true;
+            else
+                return
+                    false;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
